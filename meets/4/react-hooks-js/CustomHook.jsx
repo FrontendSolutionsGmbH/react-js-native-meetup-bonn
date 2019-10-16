03caa3e9965e225b455595f868598835fcb79fba
Torsten Stelljes <ts@froso.de>
2019-10-16 12:11:50 +0200

    better example for custom hook

--- meets/4/react-hooks-js/CustomHook.jsx
+++ meets/4/react-hooks-js/CustomHook.jsx
@@ -1,5 +1,12 @@
 () => {
-    const count = useCustomHook(1000)
+    const [interval, settInterval] = useState(1000)
 
-    return <p>{count}</p>
+    const count = useCustomHook(interval)
+
+    return <>
+        <p>Interval is: {interval} millisec</p>
+        <p>Count returned from custom hook: {count}</p>
+        <Button onClick={() => settInterval(interval => interval*10)} text="*10"/>
+        <Button onClick={() => settInterval(interval => interval/10)} text=":10"/>
+    </>
 }
